d5cb9c484536c782ca26f912181487789306bc9d
Martyna Kiljan <kmartynak@icloud.com>
2020-06-02 18:14:23 +0200

    zadanie1_funkcje

---
+++ 01/app.js
@@ -0,0 +1,14 @@
+//   function showTime() {
+//       const time = (new Date()).toLocaleDateString();
+//       console.log(time);
+//   }
+
+//   showTime()
+
+
+let showTime = function () {
+    const time = new Date().toLocaleTimeString();
+    console.log(time)
+}
+
+showTime()
