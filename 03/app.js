4fa7e3413a2ae364ccc204eff928d63ba366aacf
Martyna Kiljan <kmartynak@icloud.com>
2020-06-04 22:15:13 +0200

    zadanie03_funkcje

--- 03/app.js
+++ 03/app.js
@@ -1,14 +1,11 @@
-let sum = 0;
-
 function addNumbers(a) {
 
+    let sum = 0;
     for (i = 1; i < a + 1; i++) {
         sum = sum + i;
-        // return sum;
     }
     return sum;
 }
 
-
-addNumbers(4);
-console.log(sum)
+const res = addNumbers(4);
+console.log(res);
