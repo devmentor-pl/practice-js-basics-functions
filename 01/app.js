800af2cf5e019a35bede5eca1a478329e0d3d16d
AnnaBuller <aniabela@wp.pl>
2020-09-07 13:33:32 +0200

    solve 01 task

---
+++ 01/app.js
@@ -0,0 +1,16 @@
+// Watch your local time for next 3 seconds! ;)
+
+let counter = 1;
+
+function showTime() {
+  let time = (new Date()).toLocaleTimeString();
+  console.log(time);
+
+  if (counter === 3) {
+    clearInterval(secInterval)
+  }
+  counter++
+}
+
+
+const secInterval = setInterval(showTime, 1000)
