46b2c00ba40b0e108fba6476afa84ba84546067e
AnnaBuller <aniabela@wp.pl>
2020-09-16 12:23:22 +0200

    put func into interval

--- 04/app.js
+++ 04/app.js
@@ -1,12 +1,14 @@
 let counter = 1;
 
 const runTimer = () => {
+  let idInterval = setInterval(function () {
   let time = (new Date()).toLocaleTimeString();
   if(counter === 5) {
     clearInterval(idInterval);
   }
   console.log(time)
   counter++
+  }, 1000);
 }
 
-idInterval = setInterval(runTimer, 5000);
+runTimer()
