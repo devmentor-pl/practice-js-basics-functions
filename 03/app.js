61b8c71c2235dce903751b9baaa6798bfcf421a6
Ewa Skórska <ewaskorska94@gmail.com>
2020-12-22 19:02:33 +0000

    Add js ex 3

---
+++ 03/app.js
@@ -0,0 +1,12 @@
+function sumToGiven(n) {
+    let a = n;
+    if(typeof(a) !== 'number') return null;
+
+    let result = 0;
+    for(let i = n; i > 0; i--) {
+        result += i;
+    }
+    return result;
+}
+
+console.log(sumToGiven(4));
