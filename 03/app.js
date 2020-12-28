fbdf5b71ea9b28d859fe57194a6ab2da2ace6b10
Anna Kolodziej <kkolodziejanna@gmail.com>
2020-12-28 10:47:55 +0100

    Zad 03

--- 03/app.js
+++ 03/app.js
@@ -1,7 +1,9 @@
+let parametr = 4;
+
 const fn = function (sum) {
-    for (let i = 0; i <= parametr; i++) {
-        let x = console.log(i + parametr);
+    for (let i = 1; i <= parametr; i++) {
+        let x = console.log(sum);
     }
     return;
 }
-fn(4);
+fn();
