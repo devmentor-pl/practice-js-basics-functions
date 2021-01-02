0119cc4612e329201574b16acd48524a81e7bedd
Anna Kolodziej <kkolodziejanna@gmail.com>
2021-01-02 02:19:31 +0100

    Zad 04

---
+++ 04/app.js
@@ -0,0 +1,16 @@
+let idInterval;
+let counter = 0;
+
+const runTimer = function () {
+    console.log(new Date().toLocaleTimeString());
+    counter++;
+
+    
+    if (counter >= 5) {
+
+        clearInterval(idInterval);
+
+    }
+}
+
+idInterval = setInterval(runTimer, 1000);
