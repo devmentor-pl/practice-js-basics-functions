fe176728352d0662a7ae8172f7dcd35e1ab20092
Karolina Jedrajczyk <kalka@MacBook-Pro-Karolina.local>
2021-01-13 09:42:37 +0100

    message

--- 04/app.js
+++ 04/app.js
@@ -1,26 +1,15 @@
 function runTimer() {
+    let counter = 0;
+    const idInterval = setInterval(function() {
+        const time = (new Date()).toLocaleString();
+        console.log(time);
 
-
-    const time = (new Date()).toLocaleTimeString();
-    console.log(time);
-    }
-
-
-    setTime = setInterval(runTimer, 5000);
-
-
-
-    let idInterval;
-    let counter =1;
-
-    const showCounter = function() {
-        counter++;
-
-        if(counter > 5) {
+        counter++,
+        console.log(counter);
+        if(counter >=5) {
             clearInterval(idInterval);
-            clearInterval(setTime)
-        
         }
-    }
+    }, 5000);
+}
 
-    idInterval = setInterval(showCounter, 5000);
+runTimer();
