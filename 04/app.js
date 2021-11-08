e43c9ad14284dcdb394bf45bd3e1af9d4b176d82
Ewelina <ewelina@ekopacz.pl>
2021-11-08 20:08:02 +0100

    add setInterval funtion

---
+++ 04/app.js
@@ -0,0 +1,14 @@
+let idInterval;
+let counter = 0;
+
+
+let runTimer = function(){
+    const time = (new Date()).toLocaleTimeString();
+    console.log(time);
+    counter ++;
+    if(counter >= 5){
+        clearInterval(idInterval);
+    }
+};
+
+idInterval = setInterval(runTimer,5000);
