65f7390e138d52f0c582874501d58750d18fb6b2
Ewelina <ewelina@ekopacz.pl>
2021-11-08 09:38:51 +0100

    add anonymous function

---
+++ 03/app.js
@@ -0,0 +1,13 @@
+
+const sumNumber = function(number) {
+    let sum = 0;
+    for(let i=0; i<=number; i++){
+        sum+=i;
+} return sum;
+}
+
+const showSumNumber = sumNumber(4);
+console.log(showSumNumber);
+
+
+
