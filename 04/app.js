a660e9792e1e69a2f68ba24280e875e0ada4a447
Magdalena Nolde <twoj@email>
2021-11-13 16:51:30 +0100

    finish basic functions task

---
+++ 04/app.js
@@ -0,0 +1,12 @@
+let idInterval;
+let counter = 1;
+const runTimer = function() {
+ const time = (new Date()).toLocaleTimeString();
+ console.log(time);
+ counter++;
+ if(counter > 5) {
+ clearInterval(idInterval);
+ };
+}
+idInterval = setInterval(runTimer, 5000);
+runTimer();
