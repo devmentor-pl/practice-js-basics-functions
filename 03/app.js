2378295ae88fca9ed9c82816431d500aaee24211
Max <maciejdobisz@gmail.com>
2022-01-17 14:46:44 +0000

    practise js basic functions

--- 03/app.js
+++ 03/app.js
@@ -1,6 +1,9 @@
-let upTo= 4;
-let sum=0;
-
-for(let i=0; i<=upTo; i++) {
-    sum+=i;
+const addingUp= function(num) {
+    let sum=0;
+    for(let i=1; i<=num; i++) {
+        sum+=i;
+    }
+    return sum;
 }
+
+console.log(addingUp(4));
