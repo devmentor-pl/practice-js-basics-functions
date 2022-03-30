eb6237dadcd67e038e7148e6c4df386fdbd2c3b6
Lukasz Scisel <lukscidev@gmail.com>
2022-03-30 20:55:41 +0200

    01 done

---
+++ 01/app.js
@@ -0,0 +1,11 @@
+// function showTime() {
+//     const time = (new Date()).toLocaleTimeString();
+//     console.log(time);
+// }
+
+// showTime()
+
+; (function showTime() {
+    const time = (new Date()).toLocaleTimeString();
+    console.log(time);
+})()
