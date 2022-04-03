f27dc4a86ae748f30fac76c21fd9e5eb0220ead6
Patryk Kropop <patryk.kropop@gmail.com>
2022-04-03 18:58:22 +0200

    finished task

---
+++ 04/app.js
@@ -0,0 +1,20 @@
+function runTimer(){
+    console.log('start');
+    let counter = 0;
+    const idInterval = setInterval(function(){
+        
+        const time = (new Date()).toLocaleTimeString();
+        console.log(time);
+
+        counter++;
+        console.log(counter);
+
+        if(counter === 5){
+            clearInterval(idInterval);
+        }
+        
+    }, 1000)
+}
+
+
+runTimer();
