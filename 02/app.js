0cef8447d7724d34c8cae60e63db276a46817436
Pawel Podlesny <pawelpodlesny1196@gmail.com>
2022-04-19 17:08:32 +0200

    exercise-02-done

---
+++ 02/app.js
@@ -0,0 +1,10 @@
+const sayHello = function (firstName) {
+    console.log('Cześć '+ firstName +'!')
+}
+
+sayHello('devmentor.pl')
+
+// const sayHello = function (name) {
+//     console.log(`Cześć ${name}!`);
+// };
+// sayHello('Devmentor.pl');
