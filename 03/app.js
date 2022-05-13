b7e29e20f5bbe8623c4c44f7d9dce4a4c5cab799
Agnieszka Draganczyk <Agnieszka.127@interia.pl>
2022-05-13 18:44:12 +0200

    end task 03

---
+++ 03/app.js
@@ -0,0 +1,13 @@
+'use strict'
+
+let sum=0;
+let result= function(stop){
+    for(let i=0; i<=stop; i++){
+        
+        let res =i;
+        sum += res
+        } return sum
+}
+
+console.log(result(4))
+
