028f53aa91fca8506095a5ae932284c10bfd45c2
Szymon Ludwikowski <ludwikowski.szym1993@gmail.com>
2022-06-19 17:42:36 +0200

    task 4 done

---
+++ 04/app.js
@@ -0,0 +1,12 @@
+let timeIterator = 0;
+function runTimer() {
+	const time = new Date().toLocaleTimeString();
+	console.log(time);
+	timeIterator++;
+	console.log("timeIterator: ", timeIterator);
+	if (timeIterator === 5) {
+		clearInterval(timeInterval);
+	}
+}
+runTimer();
+const timeInterval = setInterval(runTimer, 5000);
