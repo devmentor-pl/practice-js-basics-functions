b9f54a27848bef7ce783116d9a3dc5cf59def518
Ariadna Nicieja <ariadna.nicieja@gmail.com>
2023-02-01 16:09:24 +0100

    rozwiązanie zadań kontrolnych: funkcje

---
+++ 03/app.js
@@ -0,0 +1,20 @@
+
+
+ const sumNumbers = function(number){
+
+    let result;
+
+    for( let i = 0; i<= number; i++){
+
+       i++
+       let add = i
+       result = add + i; 
+      
+    }
+
+    return(result)
+}
+
+console.log(sumNumbers(4))
+
+
---
+++ 04/app.js
@@ -0,0 +1,21 @@
+let stopInterval;
+counter = 1;
+
+
+
+function runTimer(){
+
+const time = (new Date()).toLocaleTimeString();
+console.log(time)
+
+counter++
+
+if(counter > 5){
+    clearInterval(stopInterval)
+}
+
+}
+
+stopInterval = setInterval(runTimer, 5000);
+
+
