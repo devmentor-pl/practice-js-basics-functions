41c18400ba7af37e576bd6af8c36fda9dc23ec89
Sebastian <sebastian.pawel.wapinski@gmail.com>
2023-03-15 12:33:45 +0100

    Added task 4

--- 04/app.js
+++ 04/app.js
@@ -1,13 +1,15 @@
 let stopInterval;
 let counter = 0;
 function runTimer() {
-    const time = (new Date()).toLocaleTimeString();
-    console.log(time);
-    counter++;
-
-    if(counter >= 5) {
-        clearInterval(stopInterval);
-    }
+    stopInterval = setInterval(function() {
+        const time = (new Date()).toLocaleTimeString();
+        console.log(time);
+        counter++;
+    
+        if(counter >= 5) {
+            clearInterval(stopInterval);
+        }
+    }, 5000)
 }
 
-stopInterval = setInterval(runTimer, 1000) ;
+runTimer();
