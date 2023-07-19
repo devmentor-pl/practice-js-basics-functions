b8a2dbc98cc0a8b5565f251beddd42d11e85d1fc
Fryderyk Jellinek <fryderyk.jellinek@gmail.com>
2023-07-19 16:10:49 +0200

    task 3 done

--- 01/app.js
+++ 01/app.js
@@ -1,9 +1,7 @@
-
-
 function showTime() {
-const time = (new Date()).toLocaleTimeString();
-console.clear()
-console.log(time);
+  const time = new Date().toLocaleTimeString();
+  console.clear();
+  console.log(time);
 }
 
-setInterval(showTime, 1000)
+setInterval(showTime, 1000);
---
+++ 02/app.js
@@ -0,0 +1,5 @@
+function sayHello(name) {
+  console.log(`Cześć ${name}!`);
+}
+
+sayHello("devmentor.pl");
