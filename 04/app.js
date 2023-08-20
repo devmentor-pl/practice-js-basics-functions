3a01d59b300e37e24ed666086ca36bde07a1fe4e
Hubert Wojnowski <hubertwojnowski0@gmail.com>
2023-08-20 15:41:27 +0200

    Finish 05

---
+++ 04/app.js
@@ -0,0 +1,19 @@
+function runTimer() {
+    console.log('start');
+
+    let iter = 0;
+    const idInterval = setInterval(function () {
+
+        const time = (new Date()).toLocaleTimeString();
+        console.log(time);
+
+        iter++;
+        console.log(iter);
+
+        if (iter === 6) {
+            clearInterval(idInterval);
+        }
+    }, 1000);
+}
+
+runTimer()
