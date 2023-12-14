7a23880b0eb5f2c5439c6bcf93127c397c38c46b
Dawidtk <dawidtk@gmail.com>
2023-12-14 08:43:18 +0100

    test

--- 03/app.js
+++ 03/app.js
@@ -1,5 +1,14 @@
 const getSumToEndNumber = function (number) {
     console.log(number);
+
+    let result = 0;
+    for (let i = 1; 1<= number; i++) {
+        result = result + i;
+    }
+
+    return result;
+ 
 }
 
-getSumToEndNumber(4);
+const eSum = getSumToEndNumber(4);
+console.log(eSum);
