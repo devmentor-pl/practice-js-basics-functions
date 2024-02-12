420ce8ac0865e8628563e6fd77b7fd4703527a52
Katarzyna Dworak <k.dw@poczta.onet.pl>
2024-02-12 22:04:29 +0100

    04 completed

--- 04/app.js
+++ 04/app.js
@@ -1,6 +1,17 @@
-function runTimer() {
-    const time = (new Date()).toLocaleTimeString();
-    console.log(time);
+// jest błąd - wyświetla się 5x, ale w tym samym czasie
+
+let idInterval;
+let counter = 1;
+
+const runTimer = function () {
+    for(let i = 1; i <= 5; i ++) {
+        const time = (new Date()).toLocaleTimeString();
+        console.log(time); 
+        console.log(i);
+        if(i === 5) {
+            clearInterval(idInterval);
+        }
+    }
 }
 
-runTimer();
+idInterval = setInterval(runTimer, 5000);
