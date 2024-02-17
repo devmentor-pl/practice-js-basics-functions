256bf29ecfa5a5ae25e3d00bc60c270416f3e5c6
Katarzyna Dworak <k.dw@poczta.onet.pl>
2024-02-17 07:49:39 +0100

    03 completed

--- 03/app.js
+++ 03/app.js
@@ -5,11 +5,10 @@ let sumNumbers = function (num) {
         sum = sum + i;
 
         if(i === num) {
-            // jeśli return sum jest przed console to funkcja nie wyświetla sumy
-            console.log(sum);
             return sum;
         }
     }
 }
 
 sumNumbers(4);
+console.log(sum);
