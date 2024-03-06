70a2e147049ae89eb3432f65659b46cbe5c7f36d
Jaro <jaroswidnik@interia.pl>
2024-03-06 15:14:38 +0100

    Finished Ex4 in Functions

---
+++ 04/app.js
@@ -0,0 +1,24 @@
+function runTimer() {
+
+    let x = 0
+
+
+	const parInterval = setInterval(function () {
+
+		const time = new Date().toLocaleTimeString();
+		console.log(time);
+
+        x++;
+        console.log(x);
+
+        if(x === 5) {
+            clearInterval(parInterval);
+        }
+
+
+
+	}, 1000);
+
+}
+
+runTimer();
