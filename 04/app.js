5039e8bfed75a5670f224819c7682b2e0a266c8f
Weronika Jankowska <wjankowska8@gmail.com>
2024-12-13 22:14:56 +0100

    task 4

---
+++ 04/app.js
@@ -0,0 +1,13 @@
+let counter = 1
+let idInterval
+const runTimer = () => {
+  const time = (new Date()).toLocaleTimeString();
+  console.log(time);
+  counter++
+
+  if (counter > 5) {
+    clearInterval(idInterval)
+  }
+}
+
+idInterval = setInterval(runTimer, 5000)
